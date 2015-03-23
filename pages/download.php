ef34fbda15f71c86b164fbe3bad5f4982acb9879
Andreas Bielawski <brawl98@outlook.com>
2015-03-24 00:01:02 +0100

    Neuen Installer hinzugefügt und einige Bilder entfernt (die Warnung wg. unsicherer Datei kommt nicht mehr)

--- pages/download.php
+++ pages/download.php
@@ -11,7 +11,8 @@ include("templates/navbar.php");
 	    		<div class="col-lg-12">
 					<h1><b>Downloade</b> WDD</h1>
 					<h3>WiiDataDownloader - ein Wii-Homebrew-Downloader für Windows.</h3>
-		<a class="btn btn-lg btn-danger" href="https://raw.githubusercontent.com/Brawl345/WiiDataDownloader/master/WDD%20installieren.exe" role="button">Download</a>
+		<a class="btn btn-lg btn-danger" href="https://raw.githubusercontent.com/Brawl345/WiiDataDownloader/master/WDD%20installieren.exe" role="button">Download</a><br><br>
+		<a class="btn btn-lg btn-warning" href="https://raw.githubusercontent.com/WiiDatabase/Inno-Setups/master/WiiDataDownloader/Output/WDD-setup.exe" role="button">Teste den neuen Installer</a><br>
 					<h3>(fr&uuml;he Alpha)</h3>
 	    		</div>
 	    	</div>
@@ -23,11 +24,9 @@ include("templates/navbar.php");
 	<div class="row centered">
 	    <div class="col-lg-12">
           <a href="img/wddinstaller1.png"><img class="img-responsive responsive-centered" src="img/wddinstaller1.png" alt="Führe den Installer aus"></a><br>
-          <a href="img/wddinstaller2.png"><img class="img-responsive responsive-centered" src="img/wddinstaller2.png" alt="Klicke auf Weitere Informationen"></a><br>
-          <a href="img/wddinstaller3.png"><img class="img-responsive responsive-centered" src="img/wddinstaller3.png" alt="Klicke auf Trotzdem Ausführen"></a><br>
-          <a href="img/wddinstaller4.png"><img class="img-responsive responsive-centered" src="img/wddinstaller4.png" alt="Bestätige die UAC-Warnung"></a><br>
-          <a href="img/wddinstaller5.png"><img class="img-responsive responsive-centered" src="img/wddinstaller5.png" alt="Wähle einen Installationsordner"></a><br><br>
-          <a href="img/wddinstaller6.png"><img class="img-responsive responsive-centered" src="img/wddinstaller6.png" alt="Fertig!"></a><br>
+          <a href="img/wddinstaller4.png"><img class="img-responsive responsive-centered" src="img/wddinstaller2.png" alt="Bestätige die UAC-Warnung"></a><br>
+          <a href="img/wddinstaller5.png"><img class="img-responsive responsive-centered" src="img/wddinstaller3.png" alt="Wähle einen Installationsordner"></a><br>
+          <a href="img/wddinstaller6.png"><img class="img-responsive responsive-centered" src="img/wddinstaller4.png" alt="Fertig!"></a><br>
 			</div>
 			</div>
 			</div>
